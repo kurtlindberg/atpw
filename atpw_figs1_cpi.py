f3c0cd5ef84a0412b9cd6212e2a172226fdc1877
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-08-05 17:35:25 -0400

    Update atpw_figs1_cpi.py

--- atpw_figs1_cpi.py
+++ atpw_figs1_cpi.py
@@ -1,86 +1,68 @@
-## Arctic Terrestrial Plant Waxes Supplemental Figure X
-# Plant wax Carbon Preference Index (CPI) "scatterbox"
+'''
+Manuscript title: Ecological and environmental controls on plant wax production
+and stable isotope fractionation in modern terrestrial Arctic vegetation
 
-# Ecological and environmental controls on modern plant wax production and stable isotope fractionation alogn a latitudinal transect of the Eastern Canadian Arctic
+Manuscript authors: Kurt R. Lindberg, Elizabeth K. Thomas, Martha K. Raynolds,
+Helga Bultmann, Jonathan H. Raberg
 
-# Manuscript authors: Kurt R. Lindberg, Elizabeth K. Thomas, Martha K. Raynolds, Helga Bultmann, Jonathan H. Raberg
+DOI: pending
 
-# DOI: pending
+Code author: Kurt R. Lindberg
 
-# Author: Kurt R. Lindberg
-# Last edited: 02/04/2025
+Figure S1: Plant wax CPI results
+(a) n-alkanoic acid CPI (C20-C32)
+(b) n-alkane CPI (C21-C23)
+'''
 
+# See atpw_conda_env.yml
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import atpw_functions as atpw_fun
 
+# Figure parameters for editing in Inkscape
 plt.rcParams['svg.fonttype'] = 'none'
 plt.rcParams['font.size'] = 10
 plt.rcParams['font.family'] = 'Liberation Sans'
 
+# Import plant wax data and map climate info onto it
 arc = pd.read_excel(
-  'krl_arctic_terrestrial_plantwax_20241022.xlsx',
-  sheet_name = 'plantwax'
+    'Lindberg_Arctic_terrestrial_plantwax.xlsx',
+    sheet_name = 'plantwax'
 )
 arc_data = atpw_fun.map_era5clim(arc, use_sample_year="yes")
 arc_data = atpw_fun.map_oipc(arc_data)
-# arc_data = arc_data[arc_data['growth_form'] == 'lichen']
-eca_data = arc_data[arc_data['site_name'].str.contains("AFR|CF8|3LN")].reset_index()
 
-arc_fames_cpi = atpw_fun.wax_cpi(
-  arc_data,
-  data_type="f",
-  start_chain=20,
-  end_chain=32,
-  cpi_name="fcpi_c20c32"
-)
+arc_fames_cpi = atpw_fun.wax_cpi(arc_data, data_type="f", start_chain=20, end_chain=32, cpi_name="fcpi_c20c32")
 arc_fames_cpi.replace([np.inf, -np.inf], np.nan, inplace=True)
-eca_fames_cpi = atpw_fun.wax_cpi(
-  eca_data,
-  data_type="f",
-  start_chain=20,
-  end_chain=32,
-  cpi_name="fcpi_c20c32"
-)
-eca_fames_cpi.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-arc_alks_cpi = atpw_fun.wax_cpi(
-  arc_data,
-  data_type="a",
-  start_chain=21,
-  end_chain=33,
-  cpi_name="acpi_c21c33"
-)
+arc_alks_cpi = atpw_fun.wax_cpi(arc_data, data_type="a", start_chain=21, end_chain=33, cpi_name="acpi_c21c33")
 arc_alks_cpi.replace([np.inf, -np.inf], np.nan, inplace=True)
-eca_alks_cpi = atpw_fun.wax_cpi(
-  eca_data,
-  data_type="a",
-  start_chain=21,
-  end_chain=33,
-  cpi_name="acpi_c21c33"
+
+arc_data = pd.concat(
+    [
+        arc_data,
+        arc_fames_cpi,
+        arc_alks_cpi
+    ], axis=1
 )
-eca_alks_cpi.replace([np.inf, -np.inf], np.nan, inplace=True)
-
-# print(np.nanmean(eca_fames_cpi), np.nanstd(eca_fames_cpi))
-# print(np.nanmean(eca_alks_cpi), np.nanstd(eca_alks_cpi))
-# print(np.nanmean(arc_fames_cpi), np.nanstd(arc_fames_cpi))
-# print(np.nanmean(arc_alks_cpi), np.nanstd(arc_alks_cpi))
+eca_data = arc_data[arc_data['study'] == 'Lindberg_et_al'].reset_index(drop=True)
+arc_data = arc_data[arc_data['study'] != 'Lindberg_et_al'].reset_index(drop=True)
 
 
-## Create CPI results plot
-
+# Figure script
 fig, axs = plt.subplots(2,2,layout='constrained')
 
+# n-alkanoic acid CPI (C20-C32)
 ax = axs[0,0]
 sns.scatterplot(
-  ax=ax, x=arc_data.cat, y=arc_fames_cpi,
-  hue=arc_data.lat, s=20, marker='o', palette='Blues', edgecolors='black', zorder=5
+    ax=ax, x=arc_data.cat+0.2, y=arc_data.fcpi_c20c32,
+    hue=arc_data.lat, s=20, marker='o', palette='Blues', edgecolors='black', zorder=5
 )
 sns.scatterplot(
-  ax=ax, x=eca_data.cat, y=eca_fames_cpi,
-  hue=eca_data.lat, s=20, marker='s', palette='Blues', edgecolors='black', zorder=10
+    ax=ax, x=eca_data.cat-0.2, y=eca_data.fcpi_c20c32,
+    hue=eca_data.lat, s=15, marker='D', palette='Blues', edgecolors='black', zorder=10
 )
 ax.hlines(y=1, xmin=0, xmax=9, linestyle='-', color='black', linewidth=1, zorder=1)
 ax.hlines(y=2, xmin=0, xmax=9, linestyle='--', color='black', linewidth=1, zorder=1)
@@ -88,23 +70,24 @@ ax.set_xlim([0,9])
 ax.set_ylim([0.5,150])
 ax.set_yscale('log')
 ax.set_xticks(
-  ticks=[1,2,3,4,5,6,7,8],
-  labels=["Tree","Shrub","Forb","Fern","Grass","Moss","Liverwort","Lichen"],
-  rotation=45
+    ticks=[1,2,3,4,5,6,7,8],
+    labels=["Tree","Shrub","Forb","Fern","Grass","Moss","Liverwort","Lichen"],
+    rotation=45
 )
 # ax.set_yticks()
 ax.set_xlabel("")
 ax.set_ylabel("CPI (C20-C32)")
 ax.legend('', frameon=False)
 
+# n-alkane CPI (C21-C33)
 ax = axs[0,1]
 sns.scatterplot(
-  ax=ax, x=arc_data.cat, y=arc_alks_cpi,
-  hue=arc_data.lat, s=20, marker='o', palette='Blues', edgecolors='black', zorder=5
+    ax=ax, x=arc_data.cat+0.2, y=arc_data.acpi_c21c33,
+    hue=arc_data.lat, s=20, marker='o', palette='Blues', edgecolors='black', zorder=5
 )
 sns.scatterplot(
-  ax=ax, x=eca_data.cat, y=eca_alks_cpi,
-  hue=eca_data.lat, s=20, marker='s', palette='Blues', edgecolors='black', zorder=10
+    ax=ax, x=eca_data.cat-0.2, y=eca_data.acpi_c21c33,
+    hue=eca_data.lat, s=15, marker='D', palette='Blues', edgecolors='black', zorder=10
 )
 ax.hlines(y=1, xmin=0, xmax=9, linestyle='-', color='black', linewidth=1, zorder=1)
 ax.hlines(y=2, xmin=0, xmax=9, linestyle='--', color='black', linewidth=1, zorder=1)
@@ -114,15 +97,16 @@ ax.set_xlim([0,9])
 ax.set_ylim([0.5,150])
 ax.set_yscale('log')
 ax.set_xticks(
-  ticks=[1,2,3,4,5,6,7,8],
-  labels=["Tree","Shrub","Forb","Fern","Grass","Moss","Liverwort","Lichen"],
-  rotation=45
+    ticks=[1,2,3,4,5,6,7,8],
+    labels=["Tree","Shrub","Forb","Fern","Grass","Moss","Liverwort","Lichen"],
+    rotation=45
 )
 # ax.set_yticks()
 ax.set_xlabel("")
 ax.set_ylabel("CPI (C21-C33)")
 ax.legend('', frameon=False)
 
+# Create color scale legend
 norm = plt.Normalize(arc_data['lat'].min(), arc_data['lat'].max())
 sm = plt.cm.ScalarMappable(cmap='Blues', norm=norm)
 sm.set_array([])
@@ -132,5 +116,6 @@ ax.figure.colorbar(sm, ax=axs[0,1], location='right', shrink=0.8)
 fig.delaxes(axs[1,0])
 fig.delaxes(axs[1,1])
 
-sfigure_cpi = plt.gcf()
-sfigure_cpi.savefig('figures/atpw_sfigure_cpi.svg')
+
+figs1_cpi = plt.gcf()
+# figs1_cpi.savefig('figures/atpw_figs1_cpi.svg')
